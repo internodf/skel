d74aae3f370a654d8ab6feeca0e5616fc62ea139
internodf <internodf@gmail.com>
2017-11-13 18:43:26 -0200

    FUncionando

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import {
+    routes
+} from './routes.js';
+
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.css'
+
+// import 'jquery/dist/jquery.js';
+// import 'bootstrap/dist/js/bootstrap.js'
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: routes,
+    mode: 'history'
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router: router,
+    render: h => h(App)
 })
