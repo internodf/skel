60021631b7a034099929f487ab6c19d4484583da
internodf <internodf@gmail.com>
2017-11-13 14:13:28 -0200

    Add routes e menu

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="menu-lateral" class="col-sm-2">
+      <menu-lateral :rotas="routes" :id="id"/>
+    </div>
+    <div class="col-sm-10">
+      <transition name="pagina">
+        <router-view></router-view>
+      </transition>
+  </div>
   </div>
 </template>
 
 <script>
+import { routes } from "./routes";
+
+import MenuLateral from "./components/home/MenuLateral.vue";
+import DemandaService from "./model/service/Service";
+
 export default {
-  name: 'app',
-  data () {
+  components: {
+    "menu-lateral": MenuLateral
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Demanda AE",
+      routes: routes.filter(route => route.menu)
+    };
+  },
+  created() {},
+  methods: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
 }
 
-h1, h2 {
-  font-weight: normal;
+.pagina-enter,
+.pagina-leave-to {
+  opacity: 0;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.pagina-leave,
+.pagina-enter-to {
+  opacity: 0;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 100ms;
 }
 
-a {
-  color: #42b983;
+.container-full {
+  margin: 0 auto;
+  width: 100%;
+}
+
+.cabecalho-modulo {
+  font-size: 200%;
+  font-weight: bold;
 }
 </style>
