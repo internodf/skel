6084f010c0c6c5bb6921d07284b8837c4b6499a1
internodf <internodf@gmail.com>
2017-11-14 10:08:53 -0200

    Correções menores

--- src/components/home/MenuLateral.vue
+++ src/components/home/MenuLateral.vue
@@ -4,7 +4,7 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
       <!-- Sidebar user panel -->
-      <!-- <div class="user-panel">
+      <div class="user-panel">
         <div class="pull-left image">
           <img :src="imgfolder + 'user2-160x160.jpg'" class="img-circle" alt="User Image">
         </div>
@@ -13,11 +13,11 @@
           <a href="#">
             <i class="fa fa-circle text-success"></i> Online</a>
         </div>
-      </div> -->
+      </div>
       <!-- search form -->
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <input type="text" name="q" class="form-control" placeholder="Pesquisar...">
           <span class="input-group-btn">
             <button type="submit" name="search" id="search-btn" class="btn btn-flat">
               <i class="fa fa-search"></i>
